6bda83a510f84f2d34a494f738dbcb253e3dc22b
carlos siches <carlos.siches@xagent.com>
2019-08-25 01:30:16 -0500

    MONGO CONFIG

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -6,9 +6,11 @@
 
 process.env.PORT = process.env.PORT || 3000;
 
-
 // Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || "dev"
+
+// Entorno
+
 
 
 //Base de datos
@@ -17,9 +19,12 @@ let urlDB
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe"
 } else {
-    urlDB = "mongodb+srv://mongodb_service:PlZDdao6xc3JhxSQ@cluster0-vzarv.mongodb.net/cafe"
+    urlDB = process.env.MONGO_URI
 }
 
-urlDB = "mongodb+srv://mongodb_service:PlZDdao6xc3JhxSQ@cluster0-vzarv.mongodb.net/cafe"
+//urlDB = process.env.MONGO_URI
+
+console.log(urlDB);
+
 process.env.URLDB = urlDB
 
